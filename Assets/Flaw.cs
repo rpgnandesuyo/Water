3066c5b27f673f985150dfbd9b0805cbf48b7d30
tatu1105 <tattun1105@gmail.com>
2020-02-07 16:02:21 +0900

    シーンの変更、バブルレイヤーの追加

---
+++ Assets/Flaw.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flaw : MonoBehaviour
+{
+    [SerializeField]
+    private float bubbleDelay;
+    [SerializeField]
+    GameObject bubble;
+
+    private float bubbleInstant = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(bubbleInstant >= bubbleDelay)
+        {
+            Instantiate(bubble,gameObject.transform);
+            bubbleInstant = 0;
+        }
+        bubbleInstant += Time.deltaTime;
+    }
+}
